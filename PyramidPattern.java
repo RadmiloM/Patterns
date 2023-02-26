8a595a24e7ce7d3ca9e00d8981ef1e14a2ab51b4
RadmiloM <markovicradmilo35@gmail.com>
2023-02-26 14:13:45 +0100

    reversed pyramid pattern created and printed in main.

---
+++ PyramidPattern.java
@@ -0,0 +1,22 @@
+public class PyramidPattern {
+
+    public static void displayReversePyramidPattern(int n) {
+        if (n == 1) {
+            System.out.println("*");
+            return;
+        }
+        int index = 0;
+        for (int i = n; i > 0; i--) {
+            System.out.print(" ".repeat(index++) + "* ".repeat(i) + "\n");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        displayReversePyramidPattern(1);
+    }
+}
+
+
+
+
